9be44145d61c4572435a90d114d3573d986521c9
Phu Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2025-04-17 01:03:00 +0700

    Revert "Refactor security group module name and remove redundant security group resources"
    This reverts commit 75cc73ab3328898bc4e09a00be80b9964339a8c1.

--- outputs.tf
+++ outputs.tf
@@ -5,15 +5,3 @@ output "instance_ami" {
 output "instance_arn" {
  value = aws_instance.blog.arn
 }
-
-output "vpc_id" {
-  value = module.blog_vpc.vpc_id
-}
-
-output "public_subnets" {
-  value = module.blog_vpc.public_subnets
-}
-
-output "instance_public_ip" {
-  value = aws_instance.blog.public_ip
-}
