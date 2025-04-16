730815939198661127dbd8d8eb54cbd85b345df6
Phu Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2025-04-17 01:10:50 +0700

    Reapply changing region
    This reverts commit 9be44145d61c4572435a90d114d3573d986521c9.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami*"]
   }
 
   filter {
@@ -11,71 +11,50 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = ["aws-marketplace"]
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.azs
+  public_subnets  = var.public_subnet_cidrs
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "LearnTerraform"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  name    = "blog"
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["https-443-tcp", "http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow HTTP and HTTPS in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
 }
