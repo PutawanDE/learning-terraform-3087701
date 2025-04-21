25ce5717b2161364c5e185d8fa9b8ae2462b4c83
Phu Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2025-04-22 00:26:40 +0700

    Attempt to fix target_groups config

--- main.tf
+++ main.tf
@@ -54,26 +54,23 @@ module "alb" {
   subnets          = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
-      name_prefix      = "blog"
+  target_groups = {
+    blog-target = {
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
+      target_id        = aws_instance.blog.id
     }
-  ]
+  }
 
   listeners = {
-    http_tcp_listeners = {
+    http-tcp-listeners = {
         port               = 80
         protocol           = "HTTP"
-        target_group_index = 0
+        forward = {
+          target_group_key = "blog-target"
+        }
       }
   }
 
