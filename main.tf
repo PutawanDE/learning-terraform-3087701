7b5953a97607d17a8b56efc4d1c449cbb67c1d2c
Phu Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2025-04-25 00:00:29 +0700

    Refactor blog VPC module to use variable for availability zones

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs             = ["ap-southeast-7a", "ap-southeast-7b", "ap-southeast-7c"]
+  azs             = var.azs
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"] 
 
   enable_nat_gateway = true
